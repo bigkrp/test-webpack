b7fa2204955db9c36bd2694d33bb7b0a28a24975
Bogdan Karpus <bigkrp@gmail.com>
2015-12-07 16:45:13 +0200

    7.1 урок

--- frontend/main.js
+++ frontend/main.js
@@ -1,19 +1,22 @@
 'use strict';
 
-import Menu from './menu';
+require.ensure([], function(require){
 
-let pandaMenu = new Menu({
-	title: "Меню панды",
-	items: [{
-			text: 'Яйца',
-			href: '#eggs'
-		}, {
-			text: 'Мясо',
-			href: '#meat'
-		}, {
-			text: '99% еды - бамбук!',
-			href: '#bamboo'
-		}]
+	let Menu = require('./menu');
+
+	let pandaMenu = new Menu({
+		title: "Меню панды",
+		items: [{
+				text: 'Яйца',
+				href: '#eggs'
+			}, {
+				text: 'Мясо',
+				href: '#meat'
+			}, {
+				text: '99% еды - бамбук!',
+				href: '#bamboo'
+			}]
+	});
+
+	document.body.appendChild(pandaMenu.elem);
 });
-
-document.body.appendChild(pandaMenu.elem);
